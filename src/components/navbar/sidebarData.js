9df427c4b6f6372aae2202ffa159f60cb8646da6
coffmanjrp <coffmanjrp@gmail.com>
2021-02-26 15:59:17 -0800

    Create sidebarData

--- src/components/navbar/sidebarData.js
+++ src/components/navbar/sidebarData.js
@@ -1,3 +1,88 @@
-const sidebarData = [];
+import * as FaIcons from 'react-icon/fa';
+import * as AiIcons from 'react-icon/ai';
+import * as IoIcons from 'react-icon/io';
+import * as RiIcons from 'react-icon/ri';
+
+const sidebarData = [
+  {
+    title: 'Overview',
+    path: '/overview',
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      {
+        title: 'Users',
+        path: '/overview/users',
+        icon: <IoIcons.IoIosPaper />,
+      },
+      {
+        title: 'Revenue',
+        path: '/overview/revenue',
+        icon: <RiIcons.RiMoneyDollarBoxFill />,
+      },
+    ],
+  },
+  {
+    title: 'Reports',
+    path: '/reports',
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      {
+        title: 'Reports',
+        path: '/reports/reports1',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav',
+      },
+      {
+        title: 'Reports 2',
+        path: '/reports/reports2',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav',
+      },
+      {
+        title: 'Reports 3',
+        path: '/reports/reports3',
+        icon: <IoIcons.IoIosPaper />,
+      },
+    ],
+  },
+  {
+    title: 'Products',
+    path: '/products',
+    icon: <FaIcons.FaCartPlus />,
+  },
+  {
+    title: 'Team',
+    path: '/team',
+    icon: <IoIcons.IoMdPeople />,
+  },
+  {
+    title: 'Messages',
+    path: '/messages',
+    icon: <FaIcons.FaEnvelopeOpenText />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      {
+        title: 'Message 1',
+        path: '/messages/message1',
+        icon: <IoIcons.IoIosPaper />,
+      },
+      {
+        title: 'Message 2',
+        path: '/messages/message2',
+        icon: <IoIcons.IoIosPaper />,
+      },
+    ],
+  },
+  {
+    title: 'Support',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle />,
+  },
+];
 
 export default sidebarData;
